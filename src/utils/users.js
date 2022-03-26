3dc862a2ea900cdd7cfee63aa16450f40c0e113e
Farès <faresboukhezna@gmail.com>
2022-03-26 17:20:45 +0100

    fix: to lower case for filter functions

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,23 +1,24 @@
 const _ = require('lodash');
 
 const filterUsersByName = (users, name) => {
-  return _.filter(users, (user) => `${user.name.first} ${user.name.last}`.includes(name));
+
+  return _.filter(users, (user) => `${user.name.first} ${user.name.last}`.toLowerCase().includes(name.toLowerCase()));
 }
 
 const filterUsersByCity = (users, city) => {
-  return _.filter(users, (user) => user.location.city.includes(city));
+  return _.filter(users, (user) => user.location.city.toLowerCase().includes(city.toLowerCase()));
 }
 
 const filterUsersByPhone = (users, phone) => {
-  return _.filter(users, (user) => user.phone.includes(phone));
+  return _.filter(users, (user) => user.phone.toLowerCase().includes(phone.toLowerCase()));
 }
 
 const filterUsersByGender = (users, gender) => {
-  return _.filter(users, (user) => user.gender.includes(gender));
+  return _.filter(users, (user) => user.gender.toLowerCase().includes(gender.toLowerCase()));
 }
 
 const filterUsersByState = (users, state) => {
-  return _.filter(users, (user) => user.location.state.includes(state));
+  return _.filter(users, (user) => user.location.state.toLowerCase().includes(state.toLowerCase()));
 }
 
 module.exports = {
