c2f0fa2fa057e049cf8e4ffa62e2276d47d62db8
Farès <faresboukhezna@gmail.com>
2022-03-26 17:18:23 +0100

    fix: contains to includes

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,23 +1,23 @@
 const _ = require('lodash');
 
 const filterUsersByName = (users, name) => {
-  return _.filter(users, (user) => `${user.name.first} ${user.name.last}`.contains(name));
+  return _.filter(users, (user) => `${user.name.first} ${user.name.last}`.includes(name));
 }
 
 const filterUsersByCity = (users, city) => {
-  return _.filter(users, (user) => user.location.city.contains(city));
+  return _.filter(users, (user) => user.location.city.includes(city));
 }
 
 const filterUsersByPhone = (users, phone) => {
-  return _.filter(users, (user) => user.phone.contains(phone));
+  return _.filter(users, (user) => user.phone.includes(phone));
 }
 
 const filterUsersByGender = (users, gender) => {
-  return _.filter(users, (user) => user.gender.contains(gender));
+  return _.filter(users, (user) => user.gender.includes(gender));
 }
 
 const filterUsersByState = (users, state) => {
-  return _.filter(users, (user) => user.location.state.contains(state));
+  return _.filter(users, (user) => user.location.state.includes(state));
 }
 
 module.exports = {
